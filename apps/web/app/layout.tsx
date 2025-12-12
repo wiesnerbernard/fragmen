17e254131b5f50723ee4e4f359420e985865b1a4
Bernard <bernard.wiesner@daaily.com>
2025-12-12 14:15:33 +0200

    style: sort imports

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,10 +1,10 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.css';
 import { ThemeProvider } from '@/components/theme-provider';
 import { ThemeToggle } from '@/components/theme-toggle';
-import Link from 'next/link';
 import { Analytics } from '@vercel/analytics/react';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import Link from 'next/link';
+import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
